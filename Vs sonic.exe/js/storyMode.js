ef59545ffedf9fee6d64c43750abde5fb152fe76
Thisisfenix <aleverafenix12345@gmail.com>
2024-09-19 22:14:07 -0500

    Delete Vs sonic.exe directory

--- Vs sonic.exe/js/storyMode.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Inicia la música al cargar la página
-    const backgroundMusic = document.getElementById('backgroundMusic');
-    backgroundMusic.play();
-});
