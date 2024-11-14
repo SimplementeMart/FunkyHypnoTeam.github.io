465aff36cff51ade176e021af157b6ca463ade84
Thisisfenix <aleverafenix12345@gmail.com>
2024-11-14 12:40:23 -0600

    pokedex añadido

---
+++ js/pokedex.js
@@ -0,0 +1,9 @@
+function navigate(direction) {
+    if (direction === 'previous') {
+        // Lógica para navegar al Pokémon o variante anterior
+        console.log("Navegando al anterior");
+    } else if (direction === 'next') {
+        // Lógica para navegar al Pokémon o variante siguiente
+        console.log("Navegando al siguiente");
+    }
+}
